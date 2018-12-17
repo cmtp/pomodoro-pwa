90a57e7574dea83d98afc08d056c35d2c5eeddfe
cmtp <christian.tola@hotmail.com>
2018-12-17 11:03:01 -0400

    add flex-layout and first structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,26 @@ import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { HomeComponent } from './home/home.component';
+import { TimerComponent } from './timer/timer.component';
+import { ActionsComponent } from './actions/actions.component';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TimerComponent,
+    ActionsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
