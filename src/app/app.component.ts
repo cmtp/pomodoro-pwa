56fb05cdf36413008f528fc26de8365891a6e916
cmtp <christian.tola@hotmail.com>
2018-12-18 16:44:28 -0400

    add support for service workers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { SwUpdate } from "@angular/service-worker";
 
 @Component({
-  selector: 'pmd-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "pmd-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'pomodoro-pwa';
+  title = "pomodoro-pwa";
+
+  constructor(private swUpdate: SwUpdate) {}
+
+  ngOnInit() {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.available.subscribe(() => {
+        if (confirm("New version available. Load New Version?")) {
+          window.location.reload();
+        }
+      });
+    }
+  }
 }
